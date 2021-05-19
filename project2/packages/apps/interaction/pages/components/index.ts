20797ca9d78983f3039ef71a58b4dd15df6225cd
seung Lee <jfmam@naver.com>
2021-05-20 00:59:59 +0900

    add scss lib

---
+++ project2/packages/apps/interaction/pages/components/index.ts
@@ -0,0 +1,7 @@
+export { default as Footer } from './footer';
+export { default as HeaderNavigation } from './HeaderNavigation';
+export { default as ProductDescription } from './ProductDescription';
+export { default as ProductInfo } from './ProductInfo';
+export { default as ProductLast } from './ProductLast';
+export { default as ProductNavigation } from './ProductNavigation';
+export { default as ProductSummary } from './ProductSummary';
